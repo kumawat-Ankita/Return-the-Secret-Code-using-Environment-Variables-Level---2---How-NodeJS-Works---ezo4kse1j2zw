5373b336ad3a82042aa77fccb7e5c3a09c6e3b26
kumawat-Ankita <145592877+kumawat-Ankita@users.noreply.github.com>
2024-01-06 21:03:09 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,17 +12,6 @@ function encryptString(str) {
   return encrypted;
 }
 
-// Write a GET route which encrypts the secret code and returns it to the client
-/*
-Sample Output: 
-HTTP Status Code: 200
-{
-  "secret": "<encrypted value>"
-}
-
-Use the encryptString function given above to encrypt the secret code
-*/
-
 app.get('/api/get-env', (req, res) => {
   // Retrieve the secret code from environment variables
   const secretCode = process.env.SECRET_CODE;
